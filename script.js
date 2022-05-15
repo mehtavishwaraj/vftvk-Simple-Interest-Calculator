3aebcae76f9ff9598281f646d0319a80e1c43404
mehtavishwaraj <105585224+mehtavishwaraj@users.noreply.github.com>
2022-05-15 15:37:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function compute()
 {
+    var p,r,t,si;
     p = document.getElementById("principal").value;
-    i = document.getElementById("principal").value;
-    r = document.getElementById("principal").value;
-    
+    r = document.getElementById("rate").value;
+    t = document.getElementById("years").value;
+    si = parseInt((p*r*t)/100 );
+    document.getElementById ('result').innerHTML = si;
     
 }
         
