e0d7009a8b48ccf496631297a1be430b13037f9d
mehtavishwaraj <105585224+mehtavishwaraj@users.noreply.github.com>
2022-05-15 15:27:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    i = document.getElementById("principal").value;
+    r = document.getElementById("principal").value;
+    
     
 }
         
